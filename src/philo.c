e538cf851ee0b1fee23c783b696d8d260aa06151
Yoel Cornamusaz <ycornamu@c1r10s4.42lausanne.ch>
2022-01-11 18:49:50 +0100

    to continue..

--- src/philo.c
+++ src/philo.c
@@ -6,7 +6,7 @@
 /*   By: ycornamu <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 21:27:02 by ycornamu          #+#    #+#             */
-/*   Updated: 2021/12/16 08:35:59 by yoel             ###   ########.fr       */
+/*   Updated: 2022/01/11 15:13:35 by ycornamu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,23 +15,23 @@
 #include <unistd.h>
 #include <sys/time.h>
 
-suseconds_t	get_ts_us(void)
+long long int	get_time_mili(void)
 {
 	struct timeval	tv;
 
 	gettimeofday(&tv, NULL);
-	return (tv.tv_usec);
+	return (tv.tv_sec * (long long int)1000 + tv.tv_usec / 1000);
 }
 
 void	tprint(int id, char *str)
 {
-	printf("%ld %d %s\n", get_ts_us(), id, str);
+	printf("%ld %d %s\n", get_time_mili(), id, str);
 }
 
 void	philo_eating(t_arg *arg)
 {
 	tprint(arg->id, "is eating");
-	usleep(arg->params->t2eat);
+	usleep(arg->params->t2eat * 1000);
 	*(arg->forkl) = 0;
 	*(arg->forkr) = 0;
 	pthread_mutex_unlock(arg->mforkl);
@@ -42,7 +42,7 @@ void	philo_eating(t_arg *arg)
 void	philo_sleeping(t_arg *arg)
 {
 	tprint(arg->id, "is sleeping");
-	usleep(arg->params->t2sleep);
+	usleep(arg->params->t2sleep * 1000);
 	arg->role = THINK;
 }
 
@@ -72,50 +72,49 @@ int 	philo_search_forks(t_arg *arg)
 	}
 	if (*(arg->forkl) == arg->id && *(arg->forkr) == arg->id)
 	{
-		arg->role = EAT;
-		return (2);
+		return (1);
 	}
-	printf("Not enought forks !\n");
+	//printf("Not enought forks !\n");
 	return (0);
 }
 
 void	*run_philo(void *arg_v)
 {
 	t_arg		*arg;
-	int			time_start;
+	char		has_forks;
+	long long 	last_eat;
 
 	arg = (t_arg *)arg_v;
 //	pthread_mutex_lock(arg->alive);
+	last_eat = get_time_mili();
 	while (1)
 	{
-//		if (arg->role == THINK)
-//		{
-			time_start = get_ts_us();
+		if (arg->role == THINK)
+		{
 			tprint(arg->id, "is thinking");
 			//printf("start: %ld\n", time_start);
 			//usleep(arg->params->t2die * 0.3);
 			while (arg->role == THINK)
 			{
-				philo_search_forks(arg);
-				if (time_start + arg->params->t2die < get_ts_us())
+				has_forks = philo_search_forks(arg);
+				if (last_eat + arg->params->t2die < get_time_mili())
 				{
-					printf("end: %ld\n", time_start + arg->params->t2die);
+					printf("end: %ld\n", last_eat + arg->params->t2die);
 					return (philo_died(arg));
 				}
+				if (has_forks)
+					arg->role = EAT;
 			}
-			arg->role = THINK;
-//		}
-//		if (arg->role == EAT)
-//			philo_eating(arg);
-		tprint(arg->id, "is eating");
-		usleep(arg->params->t2eat);
-		*(arg->forkl) = 0;
-		*(arg->forkr) = 0;
-		pthread_mutex_unlock(arg->mforkl);
-		pthread_mutex_unlock(arg->mforkr);
-//		if (arg->role == SLEEP)
-//			philo_sleeping(arg);
-		tprint(arg->id, "is sleeping");
-		usleep(arg->params->t2sleep);
+		}
+		philo_eating(arg);
+//		tprint(arg->id, "is eating");
+//		usleep(arg->params->t2eat * 1000);
+//		*(arg->forkl) = 0;
+//		*(arg->forkr) = 0;
+//		pthread_mutex_unlock(arg->mforkl);
+//		pthread_mutex_unlock(arg->mforkr);
+		philo_sleeping(arg);
+//		tprint(arg->id, "is sleeping");
+//		usleep(arg->params->t2sleep * 1000);
 	}
 }
--- src/sim.c
+++ src/sim.c
@@ -6,12 +6,13 @@
 /*   By: ycornamu <marvin@42lausanne.ch>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/12/14 20:59:35 by ycornamu          #+#    #+#             */
-/*   Updated: 2021/12/16 08:43:57 by yoel             ###   ########.fr       */
+/*   Updated: 2022/01/11 15:10:38 by ycornamu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "philo.h"
 #include <unistd.h>
+#include <stdio.h>
 
 static int	init_philos(pthread_t *philo, t_params *params)
 {
@@ -55,6 +56,8 @@ int	run_sim(pthread_t *philo, t_params *params)
 //		pthread_join(philo[i++], NULL);
 
 	pthread_join(philo[0], NULL);
+	pthread_join(philo[1], NULL);
+	pthread_join(philo[2], NULL);
 	return (0);
 }
 
